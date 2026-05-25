063887a125d3f3e0c465494c5e3a7ea44f7bea7f
Leenok <eseredich8@gmail.com>
2026-05-25 22:05:13 +0300

    feat:  update PokemonCard

--- rs-react-app/src/components/PokemonCard/PokemonCard.tsx
+++ rs-react-app/src/components/PokemonCard/PokemonCard.tsx
@@ -1,4 +1,5 @@
 import { type PokemonDetail } from '../../types/pokemon';
+import { useSelectedItemsStore } from '../../store/selecteditemsstore';
 import styles from './PokemonCard.module.css';
 
 interface PokemonCardProps {
@@ -8,15 +9,40 @@ interface PokemonCardProps {
 
 const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon, onClick }) => {
     const types = pokemon.types.map(t => t.type.name).join(', ');
+    const isSelected = useSelectedItemsStore((s) => s.isSelected(pokemon.id));
+    const selectItem = useSelectedItemsStore((s) => s.selectItem);
+    const unselectItem = useSelectedItemsStore((s) => s.unselectItem);
+
+    const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.stopPropagation();
+        if (e.target.checked) {
+            selectItem(pokemon);
+        } else {
+            unselectItem(pokemon.id);
+        }
+    };
+
+    const handleCheckboxClick = (e: React.MouseEvent) => {
+        e.stopPropagation();
+    };
 
     return (
         <div
-            className={`${styles.card} ${onClick ? styles.clickable : ''}`}
+            className={`${styles.card} ${onClick ? styles.clickable : ''} ${isSelected ? styles.selected : ''}`}
             onClick={onClick}
             role={onClick ? 'button' : undefined}
             tabIndex={onClick ? 0 : undefined}
             onKeyDown={onClick ? (e) => e.key === 'Enter' && onClick() : undefined}
         >
+            <div className={styles.checkboxWrapper} onClick={handleCheckboxClick}>
+                <input
+                    type="checkbox"
+                    className={styles.checkbox}
+                    checked={isSelected}
+                    onChange={handleCheckboxChange}
+                    aria-label={`Select ${pokemon.name}`}
+                />
+            </div>
             <div className={styles.imageContainer}>
                 <img
                     src={pokemon.sprites.front_default || 'https://via.placeholder.com/150'}
